832f4a09ea3a9d71aa19942cde2c431be2fec730
ZsgsDesign <zsgsdesign@gmail.com>
2019-08-10 01:45:15 +0800

    add tests

---
+++ tests/test.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__ . '/../vendor/autoload.php';
+    use MOSS\MOSS;
+    $userid = ""; // Enter your MOSS userid
+    $moss = new MOSS($userid);
+    $moss->setLanguage('java');
+    $moss->addByWildcard('test/codes/*');
+    $moss->addBaseFile('codes/Example.java');
+    $moss->setCommentString("This is a test");
+    echo json_decode($moss->send(), JSON_PRETTY_PRINT);
